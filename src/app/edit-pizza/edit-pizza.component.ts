26c8ef8d591d0702ca327409b8c261de9d423175
erivelto21 <eri_ximenes@hotmail.com>
2020-03-10 20:34:15 -0300

    anchor problem in nav pizza types fixed

--- src/app/edit-pizza/edit-pizza.component.ts
+++ src/app/edit-pizza/edit-pizza.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
 import { AlertService } from '../services/alert.service';
 import { EditPizzaService } from '../services/edit-pizza.service';
