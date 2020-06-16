d572baec625ec1ac1cbfe7d427b611a827910613
erivelto21 <eri_ximenes@hotmail.com>
2020-06-16 13:48:04 -0300

    enabled anchor scrolling for ranking and stores

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,7 +27,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled',
+    onSameUrlNavigation: 'reload',
+    scrollPositionRestoration: 'enabled'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
